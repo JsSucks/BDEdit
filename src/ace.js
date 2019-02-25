8410b45eb30d66ab1229c1154d1011d89b19cadf
Jiiks <jiiks@windowslive.com>
2019-02-25 20:20:04 +0200

    move extensions to ace_ext

--- src/ace.js
+++ src/ace.js
@@ -1,9 +1,4 @@
 export * from 'ace-builds';
-import 'ace-builds/src-noconflict/ext-language_tools';
-import 'ace-builds/src-noconflict/ext-searchbox';
-import 'ace-builds/src-noconflict/ext-options';
-import 'ace-builds/src-noconflict/ext-settings_menu';
-import 'ace-builds/src-noconflict/ext-keybinding_menu';
 import './ace_themes';
 import './ace_modes';
 import './ace_ext';
--- src/ace_ext.js
+++ src/ace_ext.js
@@ -1,1 +1,6 @@
 import 'ace-builds/src-noconflict/ext-themelist';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-searchbox';
+import 'ace-builds/src-noconflict/ext-options';
+import 'ace-builds/src-noconflict/ext-settings_menu';
+import 'ace-builds/src-noconflict/ext-keybinding_menu';
